61a059d93392ebe00a0fd010e30c970108323004
Owen-Keller <okelle01@rams.shepherd.edu>
2025-01-29 19:29:24 -0500

    Done with week 3, back to personal projects

--- Projects/main.py
+++ Projects/main.py
@@ -8,6 +8,26 @@ import my_modules
 def main():
     mm = my_modules.my_Modules()
     mm.recursive_Math(990)
+    
+    #week 3 content
+    print("\n\n\nWeek 3")
+    ### print list and tuple
+    print(mm.list,"\n",mm.tuple)
+    #retrieve the 3rd object in the tuple and list using the index value
+    print("Index of 2.\n3rd Object in the Tuple and List >> ",mm.Return_index(2))
+
+    #technically not sorting the list but It is a cool concept pushing the theory of multiple dimensions and infinite worlds.  
+    print("Random >> ",mm.bogo_sort())
+
+    ###TUPLES CAN NOT HAVE ELEMENTS REMOVED!?! ### CLEARLY I know lol ;) ;) ####
+    mm.add_obj("cheese burger") # I got a bit hungry
+
+    print("new {}th object added >> {}".format(len(mm.list),mm.list))
+
+    mm.remove_obj(0) #good news i had dinner!
+
+    print("new list with {} elements >> {}".format(len(mm.list), mm.list))
+
 
 
 
--- Projects/my_modules.py
+++ Projects/my_modules.py
@@ -11,7 +11,8 @@ class my_Modules:
 
     def __init__(self):
         self.mp = multiprocessing
-
+        self.list = ["A","B","C","D","E","F","G","H","I","J"]
+        self.tuple = ("J","I","H","G","F","E","D","C","B","A")
 
     
     def Count(self, number:int):
@@ -36,5 +37,43 @@ class my_Modules:
         
         print("{} :: {}".format(number,(number -1)/number))
         return self.recursive_Math(number-1)
+    
+
+    def sort_List(self):
+
+        return sorted(self.tuple)
+    
+    def Return_index(self, x:int):
+        return self.tuple[x], self.list[x]
+    
+    def add_obj(self, x:object):
+        self.list.append(x)
+        #cant append tyo a tuple they are not changeable only iterable
+    
+    def remove_obj(self, x:int):
+        #cant remove items from Tuple
+        try:
+            self.list.pop(x)
+        except:
+            print("Index out of range")
+
+    def bogo_sort(self):
+        lst = []
+        x = len(self.list) -1
+        
+        while True:
+            Object = self.list[random.randint(0, x)]
+
+            if Object in lst:
+                continue
+
+            lst.append(Object)
+
+            if len(lst) == 10:
+                break
+
+
+        return lst
+
 
     ###End###
